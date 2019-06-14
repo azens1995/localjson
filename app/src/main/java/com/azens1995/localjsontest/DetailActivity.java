71f79e3908aef246e7a2409d8c4bbc012bb18dbc
Azens Eklak <azens1995@gmail.com>
2019-06-14 13:53:20 +0545

    feat: add collapsing toolbar

--- app/src/main/java/com/azens1995/localjsontest/DetailActivity.java
+++ app/src/main/java/com/azens1995/localjsontest/DetailActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.transition.Explode;
 
 import com.azens1995.localjsontest.databinding.ActivityDetailBinding;
+import com.azens1995.localjsontest.databinding.CustomCollapsingBinding;
 
 import static com.azens1995.localjsontest.MainActivity.PERSON_DETAIL;
 
@@ -15,8 +16,11 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityDetailBinding detailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
+        CustomCollapsingBinding detailBinding = DataBindingUtil.setContentView(this, R.layout.custom_collapsing);
+
         PersonModel person = (PersonModel) getIntent().getSerializableExtra(PERSON_DETAIL);
+        detailBinding.collapsingToolbar.setTitle(person.getName()+' '+person.getSurname());
+        detailBinding.collapsingToolbar.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
         detailBinding.setPerson(person);
 
         /*getWindow().setSharedElementExitTransition(new Explode());*/
